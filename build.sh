5ee422823acfa8bea6553bec7a7d577795f8fa0c
zoddog <zoddog@users.noreply.github.com>
2020-10-23 22:24:07 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -19,20 +19,9 @@ cat \
 	source/main.js \
 	> build/underrun.js
 
-node shrinkit.js build/underrun.js > build/underrun.compact.js
-
-./node_modules/uglify-es/bin/uglifyjs build/underrun.compact.js \
-	--compress --screw-ie8 --mangle toplevel -c --beautify --mangle-props regex='/^_/;' \
-	-o build/underrun.min.beauty.js
-	
-./node_modules/uglify-es/bin/uglifyjs build/underrun.compact.js \
-	--compress --screw-ie8 --mangle toplevel --mangle-props regex='/^_/;' \
-	-o build/underrun.min.js
-
-
 rm build/underrun.zip
 
-sed -e '/GAME_SOURCE/{r build/underrun.min.js' -e 'd}' source/html-template.html > underrun.html
+sed -e '/GAME_SOURCE/{r build/underrun.js' -e 'd}' source/html-template.html > underrun.html
 zip -9 build/underrun.zip m/q2.png m/l1.png m/l2.png m/l3.png underrun.html
 ls -la build/
 mv underrun.html build/underrun.html
